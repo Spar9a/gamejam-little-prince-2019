5f4cff8ebb2d353fced879b5c73ece70fd3ae647
Aksimemor <spar9a@gmail.com>
2019-04-20 00:52:03 +0300

    Add DOTween framework; Sorted folders and scripts;

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Transform Earth;
+
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float timeMultiple = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Earth.Rotate(new Vector3(0,0, (speed * -1)));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Earth.Rotate(new Vector3(0,0, speed));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Earth.Rotate(new Vector3((speed * -1),0, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Earth.Rotate(new Vector3(speed,0, speed));
+        }
+    }
+}
